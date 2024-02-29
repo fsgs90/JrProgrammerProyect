7884e207d91dafe3adb97ab6c68d702670ef529a
fsgs90 <90289681+fsgs90@users.noreply.github.com>
2024-02-28 20:43:41 -0800

    Update Animal.cs
    Corrected Animal script

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
+    string animalName = "";
+    int animalSpeed = 0;
+    int animalHealth = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Animal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         
     }
 }
