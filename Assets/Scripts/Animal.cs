24819bbcdefe37aa3e3503aaee9f31406ffea7fc
fsgs90 <90289681+fsgs90@users.noreply.github.com>
2024-02-29 12:35:18 -0800

    Animal script updates text
    Seems bugged due to the font, I will switch to regular font just to make it simple.

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Animal : MonoBehaviour
 {
-    string animalName = "";
-    int animalSpeed = 0;
-    int animalHealth = 0;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    string animalName = "Animal";
+    int animalSpeed = 5;
+    int animalHealth = 5;
+
+    public Text nameText;
+    public Text healthText;
+    public Text speedText;
 
     // Update is called once per frame
     void Update()
     {
-         
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("The left mouse button was pressed");
+            UpdateDisplay();
+        } 
+    }
+
+    public virtual void UpdateDisplay()
+    {
+        nameText.text = ("Name: " + animalName);
+        healthText.text = "Health: 5";
+        speedText.text = "Speed: 5";
     }
 }
